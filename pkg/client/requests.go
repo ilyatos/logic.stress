016971476ad016c6a5bbb4ad0ac720b55e9e3e4b
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-02-26 19:06:00 +0300

    Add GetUser, StartLab, StopLab and GetLabStatus functions

--- pkg/client/requests.go
+++ pkg/client/requests.go
@@ -5,7 +5,7 @@ import (
 )
 
 func (c *Client) Index() (*Info, error) {
-	req, err := c.newRequest(http.MethodGet, "/", nil)
+	req, err := c.newRequest(http.MethodGet, "/", nil, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -13,4 +13,48 @@ func (c *Client) Index() (*Info, error) {
 	var message *Info
 	_, err = c.do(req, &message)
 	return message, err
+}
+
+func (c *Client) GetUser(subdomain string) (*User, error) {
+	req, err := c.newRequest(http.MethodGet, "/users/"+subdomain, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{
+		Subdomain: subdomain,
+	}
+	_, err = c.do(req, &user)
+	return user, err
+}
+
+func (c *Client) StartLab(startLab *LabStart) error {
+	req, err := c.newRequest(http.MethodPost, "/labs/start", startLab, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.do(req, nil)
+	return err
+}
+
+func (c *Client) StopLab(user *User) error {
+	req, err := c.newRequest(http.MethodPost, "/labs/stop", user, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.do(req, nil)
+	return err
+}
+
+func (c *Client) GetLabStatus(user *User) (*LabStatus, error) {
+	req, err := c.newRequest(http.MethodGet, "/labs/status", nil, map[string]string{"subdomain": user.Subdomain})
+	if err != nil {
+		return nil, err
+	}
+
+	var labStatus *LabStatus
+	_, err = c.do(req, &labStatus)
+	return labStatus, err
 }

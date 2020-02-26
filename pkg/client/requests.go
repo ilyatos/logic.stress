6671b2620aea8939c1a96b449f6cca093d90f7a5
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-02-26 12:37:24 +0300

    Reorganize pkg structure

--- pkg/client/requests.go
+++ pkg/client/requests.go
@@ -1,18 +1,16 @@
-package requests
+package client
 
 import (
-	"logic-stress/pkg"
-	"logic-stress/pkg/responses"
 	"net/http"
 )
 
-func (c *pkg.Client) Index() (*responses.Message, error) {
+func (c *Client) Index() (*Message, error) {
 	req, err := c.newRequest(http.MethodGet, "/", nil)
 	if err != nil {
 		return nil, err
 	}
 
-	var message *responses.Message
+	var message *Message
 	_, err = c.do(req, message)
 	return message, err
 }
--- pkg/client/responses.go
+++ pkg/client/responses.go
@@ -1,4 +1,4 @@
-package responses
+package client
 
 type Message struct {
 	Message string `json:"message"`

0c4aa214246f9e3dae76ece07390cba4ac09a58a
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-02-28 12:04:30 +0300

    Add launch argument to track the launch number

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -5,8 +5,8 @@ import (
 	"github.com/ilyatos/logic.stress/pkg/client"
 )
 
-func PrintLabState(u *client.User, ls *client.LabStatus) {
-	fmt.Printf("User: %s; \n\tState: %s; \n\tStatus %d\n", u.Subdomain, ls.State, ls.Status)
+func PrintLabState(u *client.User, launch int, ls *client.LabStatus) {
+	fmt.Printf("User: %s and Launch: %d \n\tState: %s \n\tStatus %d\n", u.Subdomain, launch, ls.State, ls.Status)
 }
 
 func PrintStructureWithFields(v interface{}) {
